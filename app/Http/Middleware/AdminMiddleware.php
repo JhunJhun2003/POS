5d59979a5a1969aa1b59e72ff63ae66e466dda8d
JhunJhun2003 <kokyaw3482@gamil.com>
2026-03-01 21:21:02 +0630

    make the code for validate admin

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -15,6 +16,9 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->user_type == 'admin') {
+            return $next($request);
+        };
+        abort(401,'unauthorized Access');
     }
 }
